7048051785cc924d47d93cb5ad3af91bbe014b05
Nikhil Lingala <140049165+Nikhil16722@users.noreply.github.com>
2023-08-02 12:40:03 +0530

    index.js

--- index.js
+++ index.js
@@ -30,32 +30,32 @@ document.addEventListener("keypress", function(event) {
 function makeSound(key) {
 
   switch (key) {
-    case "w":
+    case "n":
       var audio = new Audio('sounds/tom-1.mp3');
       audio.play();
       break;
 
-    case "a":
+    case "i":
       var audio = new Audio('sounds/tom-2.mp3');
       audio.play();
       break;
 
-    case "s":
+    case "k":
       var audio = new Audio('sounds/tom-3.mp3');
       audio.play();
       break;
 
-    case "d":
+    case "h":
       var audio = new Audio('sounds/tom-4.mp3');
       audio.play();
       break;
 
-    case "j":
+    case "i":
       var audio = new Audio('sounds/snare.mp3');
       audio.play();
       break;
 
-    case "k":
+    case "l":
       var audio = new Audio('sounds/crash.mp3');
       audio.play();
       break;
